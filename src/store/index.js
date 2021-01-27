6faa105101ebad2ccfee3c8dcae4a56f4d4b0a97
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-27 17:15:40 -0300

    lint

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// import compose
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
