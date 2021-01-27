3fc9489e6475b00635efb6774219d7949c49b12a
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-27 16:59:10 -0300

    Validacao email e senha, botoes habilitados, handleClick para redirecionamento

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import * as Actions from '../actions/index';
 
-function Login() {
+function Login(props) {
   const [state, setState] = useState({
     email: '',
     senha: '',
   });
+  const dispatch = useDispatch();
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
@@ -13,6 +17,39 @@ function Login() {
       [name]: value,
     });
   };
+
+  const handleClick = () => {
+    const { history } = props;
+
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email: state.email }));
+
+    dispatch(Actions.signIn(state.email));
+
+    history.push('/comidas');
+  };
+
+  const verifyEmailAndPassword = () => {
+    const regexEmail = new RegExp('.+@[A-z]+[.]com');
+    const regexPassword = new RegExp('.{7}');
+
+    if (regexEmail.test(state.email) && regexPassword.test(state.senha)) {
+      return true;
+    }
+  };
+
+  const buttonEnabled = () => (
+    <button data-testid="login-submit-btn" type="button" onClick={ handleClick }>
+      Entrar
+    </button>
+  );
+
+  const buttonDisabled = () => (
+    <button data-testid="login-submit-btn" type="button" disabled>
+      Entrar
+    </button>
+  );
 
   return (
     <form>
@@ -30,17 +67,21 @@ function Login() {
         <input
           data-testid="password-input"
           placeHolder="Insira a senha"
-          type="email"
+          type="password"
           name="senha"
           onChange={ handleChange }
           value={ state.senha }
         />
       </label>
-      <button data-testid="login-submit-btn" type="button">
-        Entrar
-      </button>
+      {verifyEmailAndPassword() ? buttonEnabled() : buttonDisabled()}
     </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
