5d6c3c15f05207a828de60143311838402c5bc2e
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-27 16:19:35 -0300

    Setando campos do forms controlados pelo componente

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [state, setState] = useState({
+    email: '',
+    senha: '',
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <label htmlFor="email">
@@ -9,6 +22,8 @@ function Login() {
           placeHolder="Insira o e-mail"
           type="text"
           name="email"
+          onChange={ handleChange }
+          value={ state.email }
         />
       </label>
       <label htmlFor="senha">
@@ -17,6 +32,8 @@ function Login() {
           placeHolder="Insira a senha"
           type="email"
           name="senha"
+          onChange={ handleChange }
+          value={ state.senha }
         />
       </label>
       <button data-testid="login-submit-btn" type="button">
