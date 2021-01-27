eb6a806b788a9dc0a2bbf5ff85f4cf8167e23186
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-27 17:13:53 -0300

    Probleminhas do Lint resolvido

--- src/services/drinkApi.js
+++ src/services/drinkApi.js
@@ -1,89 +1,56 @@
-export const searchDrinkNameRequest = (word) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${word}`)
-    .then((response) => response.json())
-    .then((result) => result);
-};
+export const searchDrinkNameRequest = (word) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${word}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchRandomDrinkRequest = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchRandomDrinkRequest = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDrinkByIdRequest = (id) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDrinkByIdRequest = (id) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=${id}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDetailedDrinkByIdRequest = (id) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
-export const searchDrinkFirstWordDrinkRequest = (word, type) => {
-  if(type && word.length > 1) return alert('Sua busca deve conter somente 1 (um) caracter')
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${word}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDetailedDrinkByIdRequest = (id) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+  .then((response) => response.json())
+  .then((result) => result);
+export const searchDrinkFirstWordDrinkRequest = (word) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${word}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDrinkByMainIngredientsRequest = (ingredient) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDrinkByMainIngredientsRequest = (ingredient) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allDrinkIngredientsRequest = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allDrinkIngredientsRequest = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allDrinkGlassesRequest = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allDrinkGlassesRequest = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allDrinkCategoriesRequest = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allDrinkCategoriesRequest = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allDrinkAlcoholicFilterRequest = () => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allDrinkAlcoholicFilterRequest = () => fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDrinkByCategory = (category) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDrinkByCategory = (category) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDrinkByAlcoholic = (alcoholic) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic${alcoholic}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDrinkByAlcoholic = (alcoholic) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic${alcoholic}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchDrinkByGlass = (glass) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchDrinkByGlass = (glass) => fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const returnSmallIngredientPhotoUrl = (IngredientesName) => {
-  return `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}-Small.png`
-}
+export const returnSmallIngredientPhotoUrl = (IngredientesName) => `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}-Small.png`;
 
-export const returnMediumIngredientPhotoUrl = (IngredientesName) => {
-  return `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}-Medium.png`
-}
+export const returnMediumIngredientPhotoUrl = (IngredientesName) => `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}-Medium.png`;
 
-export const returnLargeIngredientPhotoUrl = (IngredientesName) => {
-  return `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}.png`
-}
+export const returnLargeIngredientPhotoUrl = (IngredientesName) => `https://www.thecocktaildb.com/images/ingredients/${IngredientesName}.png`;
--- src/services/foodApi.js
+++ src/services/foodApi.js
@@ -1,74 +1,47 @@
-export const searchFoodNameRequest = (word) => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/searchFood.php?s=${word}`)
-    .then((response) => response.json())
-    .then((result) => result);
-};
+export const searchFoodNameRequest = (word) => fetch(`https://www.themealdb.com/api/json/v1/1/searchFood.php?s=${word}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchRandomFoodRequest = () => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchRandomFoodRequest = () => fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchFoodByIdRequest = (id) => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchFoodByIdRequest = (id) => fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchFoodFirstWordDrinkRequest = (word) => {
-  if(type && word.length > 1) return alert('Sua busca deve conter somente 1 (um) caracter')
-  return fetch(`https://www.themealdb.com/api/json/v1/1/searchFood.php?f=${word}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchFoodFirstWordDrinkRequest = (word) => fetch(`https://www.themealdb.com/api/json/v1/1/searchFood.php?f=${word}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchFoodByMainIngredientsRequest = (ingredient) => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchFoodByMainIngredientsRequest = (ingredient) => fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allFoodIngredientsRequest = () => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allFoodIngredientsRequest = () => fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allFoodAreasRequest = () => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allFoodAreasRequest = () => fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allFoodCategoriesRequest = () => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allFoodCategoriesRequest = () => fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const allFoodDetailedCategoriesRequest = () => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const allFoodDetailedCategoriesRequest = () => fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchFoodByCategory = (category) => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchFoodByCategory = (category) => fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const searchFoodByArea = (area) => {
-  return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${area}`)
-    .then((response) => response.json())
-    .then((result) => result);
-}
+export const searchFoodByArea = (area) => fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${area}`)
+  .then((response) => response.json())
+  .then((result) => result);
 
-export const returnIngredientPhotoUrl = (IngredientesName) => {
-  return `https://www.themealdb.com/images/ingredients/${IngredientesName}.png`
-}
+export const returnIngredientPhotoUrl = (IngredientesName) => `https://www.themealdb.com/images/ingredients/${IngredientesName}.png`;
 
-export const returnSmallIngredientPhotoUrl = (IngredientesName) => {
-  return `https://www.themealdb.com/images/ingredients/${IngredientesName}-Small.png`
-}
+export const returnSmallIngredientPhotoUrl = (IngredientesName) => `https://www.themealdb.com/images/ingredients/${IngredientesName}-Small.png`;
