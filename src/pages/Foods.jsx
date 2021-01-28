ac9d0cefa76aa2424beaade67fe4ed3267af6d3b
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-28 19:00:47 -0300

    Logica da searchbar

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MenuInferior from '../components/MenuInferior';
-import Header from '../components/Header'
+import SearchBar from '../components/SearchBar';
+import Header from '../components/Header';
 
 export default function Foods() {
+  const searchBarView = false;
   return (
     <div>
       <Header title="Comidas" />
+      {searchBarView && <SearchBar />}
       <MenuInferior />
     </div>
   );
