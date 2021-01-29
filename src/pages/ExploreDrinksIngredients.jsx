48697b51540cd46caf44ad609d514a033f375ee2
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-29 15:39:43 -0300

    renderizando footer nas rotas

--- src/pages/ExploreDrinksIngredients.jsx
+++ src/pages/ExploreDrinksIngredients.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MenuInferior from '../components/MenuInferior';
 
 function ExploreDrinksIngredients() {
-  return <div>ExploreDrinksIngredients</div>;
+  return (
+    <div>
+      ExploreDrinksIngredients
+      <MenuInferior />
+    </div>
+  );
 }
 
 export default ExploreDrinksIngredients;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MenuInferior from '../components/MenuInferior';
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      Profile
+      <MenuInferior />
+    </div>
+  );
 }
 
 export default Profile;
