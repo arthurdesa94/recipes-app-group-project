aeafaf185b2b65fe3e7267006bee1f6c17bf6ec8
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-02-03 09:20:31 -0300

    primeira page main Explorar feita

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MenuInferior from '../components/MenuInferior';
 import Header from '../components/Header';
 
@@ -6,6 +7,12 @@ function Explore() {
   return (
     <div>
       <Header title="Explorar" search={ false } />
+      <Link to="/explorar/comidas" data-testid="explore-food">
+        Explorar Comidas
+      </Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">
+        Explorar Bebidas
+      </Link>
       <MenuInferior />
     </div>
   );
