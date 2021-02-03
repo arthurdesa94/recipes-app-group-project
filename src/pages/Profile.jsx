2e48c17a14ab757b8f1b88b749cfa354e92dd88e
Arthur de Sa <arthurdesa94@gmail.com>
2021-02-02 21:54:48 -0300

    ta passando nos testes ja galera, agora vou poder ajudar mais hahah

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
-import MenuInferior from '../components/MenuInferior';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import MenuInferior from '../components/MenuInferior';
 
 function Profile() {
-  return (
-    <div>
-      <Header title="Perfil" search={ false } />
-      <MenuInferior />
-    </div>
-  );
+  const eraseLocalStorage = () => {
+    localStorage.clear();
+  };
+  const profileItems = () => {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    return (
+      <div>
+        <Header title="Profile" />
+        <h4 data-testid="profile-email">{ email }</h4>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            onClick={ () => eraseLocalStorage() }
+            data-testid="profile-logout-btn"
+          >
+          Sair
+          </button>
+        </Link>
+        <MenuInferior />
+      </div>
+    );
+  };
+return (profileItems());
 }
-
 export default Profile;
