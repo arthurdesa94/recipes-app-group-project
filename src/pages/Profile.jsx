90ff55ebee46018ee9dcb95d25e85e7bd55fa6eb
Arthur de Sa <arthurdesa94@gmail.com>
2021-02-03 14:55:07 -0300

    excluindo redundancias part2

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -13,19 +13,22 @@ function Profile() {
       <div>
         <Header title="Profile" />
         <h4 data-testid="profile-email">{ email }</h4>
-        <Link to="/receitas-feitas"
-        data-testid="profile-done-btn"
+        <Link
+          to="/receitas-feitas"
+          data-testid="profile-done-btn"
         >
           Receitas Feitas
         </Link>
-        <Link to="/receitas-favoritas"
-        data-testid="profile-favorite-btn"
+        <Link
+          to="/receitas-favoritas"
+          data-testid="profile-favorite-btn"
         >
           Receitas Favoritas
         </Link>
-        <Link to="/"
-        onClick={ () => eraseLocalStorage() }
-        data-testid="profile-logout-btn"
+        <Link
+        to="/"
+          onClick={ () => eraseLocalStorage() }
+          data-testid="profile-logout-btn"
         >
           Sair
         </Link>
