534b0472907a742da6d4686f88834b96a5111b0f
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-02-04 09:29:32 -0300

    Correcao Profile Substituicao do storage pelo redux para avaliador passar

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function Profile() {
+  const user = useSelector((state) => state.user);
   const eraseLocalStorage = () => {
     localStorage.clear();
   };
   const profileItems = () => {
-    const { email } = JSON.parse(localStorage.getItem('user'));
+    // const { email } = JSON.parse(localStorage.getItem('user'));
     return (
       <div>
-        <Header title="Profile" />
-        <h4 data-testid="profile-email">{ email }</h4>
-        <Link
-          to="/receitas-feitas"
-          data-testid="profile-done-btn"
-        >
+        <h4 data-testid="profile-email">{user.email}</h4>
+        <Link to="/receitas-feitas" data-testid="profile-done-btn">
           Receitas Feitas
         </Link>
-        <Link
-          to="/receitas-favoritas"
-          data-testid="profile-favorite-btn"
-        >
+        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
           Receitas Favoritas
         </Link>
         <Link
@@ -32,10 +27,15 @@ function Profile() {
         >
           Sair
         </Link>
-        <MenuInferior />
       </div>
     );
   };
-  return (profileItems());
+  return (
+    <div>
+      <Header title="Perfil" search={ false } />
+      {profileItems()}
+      <MenuInferior />
+    </div>
+  );
 }
 export default Profile;
