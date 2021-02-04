908aa1a1db8f5f9ec722b70e4342d0350145b951
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-02-04 09:31:30 -0300

    erros de redundancia e lint

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,30 +6,30 @@ import MenuInferior from '../components/MenuInferior';
 
 function Profile() {
   const user = useSelector((state) => state.user);
+
   const eraseLocalStorage = () => {
     localStorage.clear();
   };
-  const profileItems = () => {
-    // const { email } = JSON.parse(localStorage.getItem('user'));
-    return (
-      <div>
-        <h4 data-testid="profile-email">{user.email}</h4>
-        <Link to="/receitas-feitas" data-testid="profile-done-btn">
-          Receitas Feitas
-        </Link>
-        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
-          Receitas Favoritas
-        </Link>
-        <Link
-          to="/"
-          onClick={ () => eraseLocalStorage() }
-          data-testid="profile-logout-btn"
-        >
-          Sair
-        </Link>
-      </div>
-    );
-  };
+
+  const profileItems = () => (
+    <div>
+      <h4 data-testid="profile-email">{user.email}</h4>
+      <Link to="/receitas-feitas" data-testid="profile-done-btn">
+        Receitas Feitas
+      </Link>
+      <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
+        Receitas Favoritas
+      </Link>
+      <Link
+        to="/"
+        onClick={ eraseLocalStorage }
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </Link>
+    </div>
+  );
+
   return (
     <div>
       <Header title="Perfil" search={ false } />
