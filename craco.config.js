59954508bde395a8fab19489707ef4c12a1a8eb3
PedroMarqDev <PedroMarqDev@gmail.com>
2021-02-04 14:46:29 -0300

    Reset to food and drink details + favorite

--- craco.config.js
+++
@@ -1,9 +0,0 @@
-// craco.config.js
-module.exports = {
-  style: {
-    postcss: {
-      // eslint-disable-next-line global-require
-      plugins: [require('tailwindcss'), require('autoprefixer')],
-    },
-  },
-};
