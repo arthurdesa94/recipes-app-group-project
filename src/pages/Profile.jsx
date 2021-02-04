79a93bd1bead9590963f32ae97650bf7a4b5b7ce
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-02-04 09:37:43 -0300

    voltando pra local storage e pondo um default pro avaliador na tela profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function Profile() {
-  const user = useSelector((state) => state.user);
-
   const eraseLocalStorage = () => {
     localStorage.clear();
   };
 
-  const profileItems = () => (
-    <div>
-      <h4 data-testid="profile-email">{user.email}</h4>
-      <Link to="/receitas-feitas" data-testid="profile-done-btn">
-        Receitas Feitas
-      </Link>
-      <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
-        Receitas Favoritas
-      </Link>
-      <Link
-        to="/"
-        onClick={ eraseLocalStorage }
-        data-testid="profile-logout-btn"
-      >
-        Sair
-      </Link>
-    </div>
-  );
+  const profileItems = () => {
+    const { email } = JSON.parse(localStorage.getItem('user')) || { email: '' };
+    return (
+      <div>
+        <h4 data-testid="profile-email">{email}</h4>
+        <Link to="/receitas-feitas" data-testid="profile-done-btn">
+          Receitas Feitas
+        </Link>
+        <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">
+          Receitas Favoritas
+        </Link>
+        <Link
+          to="/"
+          onClick={ eraseLocalStorage }
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </Link>
+      </div>
+    );
+  };
 
   return (
     <div>
