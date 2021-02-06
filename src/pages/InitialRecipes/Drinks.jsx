2cd8230b1565178e7d22d90b24a68fdc48be440c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-02-06 14:28:15 -0300

    Arrumando importacao de novas pastas no appJs

--- src/pages/InitialRecipes/Drinks.jsx
+++ src/pages/InitialRecipes/Drinks.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import MenuInferior from '../components/MenuInferior';
-import SearchBar from '../components/SearchBar';
-import Header from '../components/Header';
-import RecipeDrinkCard from '../components/RecipeDrinkCard';
-import CategoryBar from '../components/CategoryBar';
-import * as Actions from '../actions/index';
+import MenuInferior from '../../components/MenuInferior';
+import SearchBar from '../../components/SearchBar';
+import Header from '../../components/Header';
+import RecipeDrinkCard from '../../components/RecipeCards/RecipeDrinkCard';
+import CategoryBar from '../../components/CategoryBar';
+import * as Actions from '../../actions/index';
 
 function Drinks({ location }) {
   const { setter, setterIngredient } = useSelector((state) => state.user);
