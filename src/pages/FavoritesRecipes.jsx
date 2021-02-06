665173dd6742eada15a5ef876e27f2332e0eebea
PedroMarqDev <PedroMarqDev@gmail.com>
2021-02-06 13:50:00 -0300

    ops

--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 function FavoriteRecipes() {
   return (
     <div>
-      <Header title="Receitas Favoritas" search={false} />
+      <Header title="Receitas Favoritas" search={ false } />
     </div>
   );
 }
